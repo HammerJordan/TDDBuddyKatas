bddfb5d9dfe18f3fc689e3a07145a530ebd1035b
Jordan <hammerleejordan@gmail.com>
2021-07-31 08:30:59 -0700

    Generate change from transaction
     a

--- HeavyMetalBakeSale/Kata/BakeSale.cs
+++ HeavyMetalBakeSale/Kata/BakeSale.cs
@@ -5,7 +5,7 @@ namespace HeavyMetalBakeSale.Kata
 {
     public class BakeSale
     {
-        private List<Item> inventory;
+        private readonly List<Item> inventory;
 
         public BakeSale(List<Item> inventory)
         {
@@ -17,43 +17,90 @@ namespace HeavyMetalBakeSale.Kata
             if (string.IsNullOrEmpty(items))
                 throw new InvalidItemException("Items must not be null or empty");
 
-            var identifiers = items.Split(",");
-            
-            if(identifiers.Any(x => x.Length != 1))
-                throw new InvalidItemException("Items identifiers must have a length of 1 seperated by a comma");
-            
-            if(!identifiers.Any(x => inventory.Any(c => c.Identifier == x[0])))
-                throw new InvalidItemException($"No Item identifier for product found");
+            var identifiers = items
+                .Split(",")
+                .ToArray();
 
-            var cart = GetItems(items);
+            ValidateItemIdentifiers(identifiers);
 
-            decimal cost = cart.Sum(x => x.Cost * x.Quantity);
-
-
-
-            return new Transaction("Message", cost, items);
+            var cart = GetItems(identifiers);
+            var transaction = CalculateTransaction(cart);
+            return transaction;
         }
 
-        private IEnumerable<Item> GetItems(string items)
+        private Transaction CalculateTransaction(IEnumerable<Item> cart)
+        {
+            var cartArray = cart as Item[] ?? cart.ToArray();
+            decimal cost = cartArray.Sum(x => x.Cost * x.Quantity);
+            string message = GenerateTransactionInvoice(cartArray, cost);
+            string products = CartToProductString(cartArray);
+            return new Transaction(message, cost, products);
+        }
+
+        private static string CartToProductString(IEnumerable<Item> cartArray)
+        {
+            string products = "";
+            foreach (var item in cartArray)
+            {
+                for (int i = 0; i < item.Quantity; i++)
+                {
+                    products += item.Identifier + ',';
+                }
+            }
+
+            return products;
+        }
+
+        private static string GenerateTransactionInvoice(IEnumerable<Item> cartArray, decimal cost)
+        {
+            string message = "Items to purchase:\n";
+            foreach (var item in cartArray)
+            {
+                message += $"{item.Name}: {item.Cost:C} (X {item.Quantity}) - {item.Cost * item.Quantity:C}";
+                message += '\n';
+            }
+
+            message += $"Total: {cost:C}";
+            return message;
+        }
+
+        private void ValidateItemIdentifiers(string[] identifiers)
+        {
+            if (identifiers.Any(x => x.Length != 1))
+                throw new InvalidItemException("Items identifiers must have a length of 1 seperated by a comma");
+
+            if (!identifiers.Any(x => inventory.Any(c => c.Identifier == x.First())))
+                throw new InvalidItemException($"No Item identifier for product found");
+        }
+
+        private IEnumerable<Item> GetItems(IEnumerable<string> identifiers)
         {
             var output = new List<Item>();
 
-            foreach (char i in items.Split(',').Select(x => x.First()))
+            foreach (char i in identifiers.Select(x => x.First()))
             {
-               var item = inventory.First(x => x.Identifier == i);
-               
-               if (output.Any(x => x.Identifier == i))
-                   output.First(x => x.Identifier == i).Quantity++;
-               else
-               {
-                   var newItem = (Item)item.Clone();
-                   newItem.Quantity = 1;
-                   output.Add(newItem);
-               }
+                var item = inventory.First(x => x.Identifier == i);
 
+                if (output.Any(x => x.Identifier == i))
+                    output.First(x => x.Identifier == i).Quantity++;
+                else
+                {
+                    var newItem = (Item) item.Clone();
+                    newItem.Quantity = 1;
+                    output.Add(newItem);
+                }
             }
 
             return output;
+        }
+
+        public TransactionReceipt MakeTransaction(Transaction transaction, decimal amountPayed)
+        {
+            // if (amountPayed < transaction.Total)
+            //     return new TransactionReceipt("Insufficient funds to make transaction",0);
+
+            return new TransactionReceipt("something", amountPayed - transaction.Total);
+
         }
     }
 }
--- HeavyMetalBakeSale/Tests/BakeSaleTests.cs
+++ HeavyMetalBakeSale/Tests/BakeSaleTests.cs
@@ -59,18 +59,28 @@ namespace HeavyMetalBakeSale.Tests
                 .Should()
                 .Throw<InvalidItemException>();
         }
-        
-        
+
         [Theory]
         [InlineData("B", .65)]
         [InlineData("B,C,M", 3)]
-
+        [InlineData("C,M", 2.35)]
+        [InlineData("W,W,W,W,W,W", 9)]
         public void ItemsToPurchase_ShouldReturnAnAppropriateTransaction_WhenInputIsValid(string items, decimal cost)
         {
             var transaction = bakeSale.ItemsToPurchase(items);
 
             transaction.Total.Should().Be(cost);
-            
+        }
+
+        [Theory]
+        [InlineData("B", .65, 0)]
+        [InlineData("B", .75, .1)]
+        [InlineData("C,M", 3.36, 1.01)]
+        public void MakeATransaction(string items, decimal amountPayed, decimal expectedChange)
+        {
+            var transaction = bakeSale.ItemsToPurchase(items);
+            var receipt = bakeSale.MakeTransaction(transaction, amountPayed);
+            receipt.ChangeDue.Should().Be(expectedChange);
         }
     }
 }
